ebb536dd8e8d0a4208a4d131fd5a9c6137c5888b
ferrimarco <116425965+MarcoFerri05@users.noreply.github.com>
2022-11-11 20:05:40 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,19 @@ let x,y,d,a,color1,color2,color3
 
 function setup()
 {
-  
+    createCanvas(w,h)
+    background("pink")
 }
 
 function draw ()
 {
+  dati(x,y,d)
+  circle(x,y,d)
+}
 
+function dati()
+{
+    x = random(0,w)
+    y = random(0,h)
+    d = random(30,90)
 }

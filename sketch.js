fd75d0cf166cb2ec688734cd6f66048089fc5e23
ferrimarco <116425965+MarcoFerri05@users.noreply.github.com>
2022-11-11 20:06:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,8 +11,9 @@ function setup()
 
 function draw ()
 {
-  dati(x,y,d)
+  dati(x,y,d,color1,color2,color3)
   circle(x,y,d)
+    fill(color1, color2, color3)
 }
 
 function dati()
@@ -20,4 +21,8 @@ function dati()
     x = random(0,w)
     y = random(0,h)
     d = random(30,90)
+    
+    color1 = random(0,255)
+    color2 = random(0,255)
+    color3 = random(0,255)
 }
